41468ba0c3f561ebe75fbb41b73566eb5a2a92ce
Simon Lagutin <lasimondep@inbox.ru>
2018-12-11 02:54:47 +0300

    Single def get_request() instead common code

--- mediator.py
+++ mediator.py
@@ -11,43 +11,34 @@ class Hosts:
 	def set_hosts(*hosts):
 		Hosts.Interface, Hosts.Facade, Hosts.Tex, Hosts.DataBase = hosts
 
+def get_request(host, req, data=None, **addhead):
+	connection = HTTPConnection(host)
+	reqID = GenReqId.next()
+	head = {"request": req, "reqID": reqID}
+	head.update(addhead)
+	connection.request("GET", '/', headers=head, body=data)
+	response = connection.getresponse()
+	connection.close()
+	return response.getheaders(), response.read()
+
 def get_taskList():
-	connection = HTTPConnection(Hosts.Interface)
-	reqID = GenReqId.next()
-	connection.request("GET", "/", headers={"request": "get_taskList", "reqID": reqID})
-	#connection.request("GET", "/list.html") debug only
-	response = connection.getresponse()
-	connection.close()
-	rheaders, rdata = response.getheaders(), response.read()
+	rheaders, rdata = get_request(Hosts.Facade, "get_taskList")
 	return rdata
 
-def get_task_Facade(serverInstance, taskID):
-	connection = HTTPConnection(Hosts.Facade)
-	reqID = GenReqId.next()
-	connection.request("GET", "/", headers={"request": "get_task", "reqID": reqID, "taskID": taskID})
-	#connection.request("GET", "/task.json") debug only
-	response = connection.getresponse()
-	connection.close()
-	rheaders, rdata = response.getheaders(), response.read()
+def get_task_Facade(task):
+	rheaders, rdata = get_request(Hosts.Facade, "get_task", taskID=task)
 	return rdata
 
-def get_Tex(taskID):
-	connection = HTTPConnection(Hosts.Tex)
-	reqID = GenReqId.next()
-	connection.request("GET", "/", headers={"request": "get_Tex", "reqID": reqID})
-	#connection.request("GET", "/task.tex") debug only
-	response = connection.getresponse()
-	connection.close()
-	rheaders, rdata = response.getheaders(), response.read()
+def get_Tex(taskFile):
+	rheaders, rdata = get_request(Hosts.Tex, "get_Tex", taskFile)
 	return rdata
 
 class MediatorHTTPRequestHandler(BaseHTTPRequestHandler):
 	def do_GET(self):
 		request, reqID = self.headers["request"], self.headers["reqID"]
-		#TODO check Fasade is runing
+		#TODO check Fasade & Tex is runing
 		if request == "get_taskList":
-			taskList = get_taskList(self.server)
-			print(taskList)
+			taskList = get_taskList()
 			self.send_response(200)
 			self.send_header("response", "get_taskList")
 			self.send_header("resID", reqID)
@@ -56,9 +47,9 @@ class MediatorHTTPRequestHandler(BaseHTTPRequestHandler):
 		if request == "get_task":
 			taskID = self.headers["taskID"]
 			#TODO request from database
-			taskFile = get_task_Facade(self.server, taskID)
-			texFile = get_Tex(self.server, taskFile)
-			send_response(200)
+			taskFile = get_task_Facade(taskID)
+			texFile = get_Tex(taskFile)
+			self.send_response(200)
 			self.send_header("response", "get_task")
 			self.send_header("resID", reqID)
 			self.send_header("Content-Type", "text/tex;charset=utf-8")
@@ -66,5 +57,5 @@ class MediatorHTTPRequestHandler(BaseHTTPRequestHandler):
 			self.wfile.write(taskFile)
 
 serv = HTTPServer(("127.0.0.1", 25800), MediatorHTTPRequestHandler)
-Hosts.set_hosts(["127.0.0.1", 25500], ["127.0.0.1", 25600], ["127.0.0.1", 25700], ["127.0.0.1", 25900])
+Hosts.set_hosts("127.0.0.1:25500", "127.0.0.1:25600", "127.0.0.1:25700", "127.0.0.1:25900")
 serv.serve_forever()
