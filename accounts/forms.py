fda080a7faa0dc8400ac801214bc4415f5558bbc
Cerberous <kurlov_99@mail.ru>
2019-04-15 22:16:41 +0300

    cleaning

--- accounts/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from accounts.models import Post
-
-class HomeForm(forms.ModelForm):
-    post = forms.CharField()
-    class Meta:
-        model = Post
-        fields = {'post',}
