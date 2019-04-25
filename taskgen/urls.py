833634ec3a7d697c176417c077f03d494a8f38ba
Sentinel <sentinel@localhost>
2019-04-25 12:29:29 +0300

    Add CSS and basic register form

--- taskgen/urls.py
+++ taskgen/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
 	path('', views.index, name='index'),
 	path('list/gen/', views.generate_list, name='generate_list'),
 	path('list/db/', views.db_list, name='db_list'),
+	path('login/', views.my_view, name='my_view'),
+	path('register/', views.my_view_reg, name='my_view_reg'),
 	path('statements/', views.statements, name='statements')
 #	path('statements/', views.statements, name='statements')
 ]
